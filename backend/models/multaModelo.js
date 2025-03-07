3bfe4f9e5ffeffb274d31db95f00353a55c61a2d
Caio de Andrade <84874942+CaioSoandrd@users.noreply.github.com>
2025-03-07 16:03:00 -0300

    modelo de multa

---
+++ backend/models/multaModelo.js
@@ -0,0 +1,26 @@
+const db = require('../config/mysql'); // Conexão com o banco de dados
+
+const Multa = {
+    create: (multa, callback) => {
+        const { data, valor, descricao, pontos, motorista_id } = multa;
+        const query = 'INSERT INTO multas (data, valor, descricao, pontos, motorista_id) VALUES (?, ?, ?, ?, ?)';
+        db.query(query, [data, valor, descricao, pontos, motorista_id], callback);
+    },
+
+    getAll: (callback) => {
+        const query = 'SELECT * FROM multas';
+        db.query(query, callback);
+    },
+
+    getById: (id, callback) => {
+        const query = 'SELECT * FROM multas WHERE id = ?';
+        db.query(query, [id], callback);
+    },
+
+    getByMotorista: (motorista_id, callback) => {
+        const query = 'SELECT * FROM multas WHERE motorista_id = ?';
+        db.query(query, [motorista_id], callback);
+    }
+};
+
+module.exports = Multa;

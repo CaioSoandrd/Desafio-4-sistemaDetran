2b3e657c9c86fb5bd76ae5e7e34ada9f15e92b59
Caio de Andrade <84874942+CaioSoandrd@users.noreply.github.com>
2025-03-08 16:37:37 -0300

    feat: getVeiculoByID e getVeiculoByMotorista
    funções de busca de veiculos por id de veiculo e por motorista

--- backend/controllers/veiculoController.js
+++ backend/controllers/veiculoController.js
@@ -39,3 +39,31 @@ exports.getAllVeiculos = (req, res) => {
         res.status(200).json(results);
     });
 };
+
+// Função para buscar um veículo pelo ID
+exports.getVeiculoById = (req, res) => {
+    // extrai o ID do veículo dos parâmetros da URL
+    const { id } = req.params;
+
+    // chama a função getById do modelo, que executa um SELECT filtrando pelo ID
+    Veiculo.getById(id, (err, results) => {
+        if (err) {
+            return res.status(500).json({ erro: "Erro ao buscar veículo" });
+        }
+        // se nenhum veículo for encontrado, retorna status 404
+        if (results.length === 0) {
+            return res.status(404).json({ erro: "Veículo não encontrado" });
+        }
+        // retorna o veículo encontrado
+        res.status(200).json(results[0]);
+    });
+};
+//função para buscar veiculos por motorista especifico
+exports.getVeiculosByMotorista = (req, res) =>{
+    // extrai o id_motorista dos parametros URL
+    const {motorista_id} =req.params;
+    //chama função do modelo para buscar veiculos filtrados pelo motorista_id
+    Veiculo.getByMotorista()
+
+}
+

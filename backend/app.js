b2585110d9a0fac158cfa096703ddef66a61e293
JoaoComTil <jgabrieldbrito@gmail.com>
2025-03-10 20:59:35 -0300

    conflito que tava dando entre duas versoes dos json

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,7 @@
 const express = require('express');
 const app = express();
 
-<<<<<<< HEAD
-app.use(express.json()); // Para parsing de JSON
-app.use(express.urlencoded({ extended: false })); // Para parsing de formulários (opcional)
 
-
-const motoristasRouter = require('../../OneDrive/Documentos/GitHub/Desafio-4-sistemaDetran/routes/motoristas');
-const multasRouter = require('../../OneDrive/Documentos/GitHub/Desafio-4-sistemaDetran/routes/multas');
-const veiculosRouter = require('../../OneDrive/Documentos/GitHub/Desafio-4-sistemaDetran/routes/veiculos');
-const testeRouter = require('./routes/teste');
-
-app.use('/motoristas', motoristasRouter);
-app.use('/multas', multasRouter);
-app.use('/veiculos', veiculosRouter);
-app.use('/api', testeRouter); // Usar a rota de teste
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
-
-=======
 // middlewares básicos
 app.use(express.json()); // para parsing de JSON
 app.use(express.urlencoded({ extended: false })); // para parsing de formulários
@@ -52,6 +34,6 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
->>>>>>> temp-branch
+
 
 module.exports = app;
